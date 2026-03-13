709b39131f036b3968af89d57085140f2b9da729
SalRuz <ruzelsalmanov1@gmail.com>
2026-03-13 22:42:54 +0500

    Delete test_imports.js

--- test_imports.js
+++
@@ -1,5 +0,0 @@
-const cv = require('canvas');
-const pv = require('prismarine-viewer');
-console.log('canvas:', cv.version || 'loaded');
-console.log('prismarine-viewer:', require('prismarine-viewer/package.json').version);
-console.log('node-canvas-webgl:', require('node-canvas-webgl/package.json').version);
